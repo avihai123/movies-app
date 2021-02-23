d293404f1326be2b95816393412b838a26a1afb2
avihai <avihai.yosef100@gmail.com>
2021-02-23 12:43:54 +0200

    create search with pagination api

--- server/server.js
+++ server/server.js
@@ -1,25 +1,41 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const movies = require('./movies.json');
+"use strict";
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
 
+const cors = require("cors");
+const movies = require("./movies.json");
 const app = express();
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('/movies', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.send(movies);
+app.get("/movies", (req, res) => {
+  const { search = "", start, end } = req.query;
+
+  // TODO: add params validation
+  // TODO: create a movie service here
+  // Maybe collect some analytics, search terms, to optimize user experience.
+
+  const searchAsLower = search.toLowerCase();
+  const matchedMovies = search
+    ? movies.filter(
+        (movie) =>
+          movie.title.toLowerCase().includes(searchAsLower) ||
+          movie.synopsis.toLowerCase().includes(searchAsLower)
+      )
+    : movies;
+
+  res.send(matchedMovies.slice(Number.parseInt(start), Number.parseInt(end)));
 });
 
-app.get('/movies/:id', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.send(movies.filter(movie => movie.id === req.params.id));
+app.get("/movies/:id", (req, res) => {
+  // should return 404 when not found
+  res.send(movies.filter((movie) => movie.id === req.params.id)[0]);
 });
 
 app.listen(3000, function () {
-    console.log(`app listening on port ${3000}!`);
+  console.log(`app listening on port ${3000}!`);
 });
 
 module.exports = app;
